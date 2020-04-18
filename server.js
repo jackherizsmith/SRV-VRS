af341f3024edce1ff61ea03fbdf0ba5b603081fe
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-04-18 11:41:02 +0100

    fic folder restructure

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const cookie = require('cookie-parser');
 
-const logger = require('./middleware/logger');
-const getUser = require('./middleware/getUser');
-const checkAuth = require('./middleware/checkAuth');
-const handleErrors = require('./middleware/handleErrors');
+const logger = require('./src/middleware/logger');
+const getUser = require('./src/middleware/getUser');
+const checkAuth = require('./src/middleware/checkAuth');
+const handleErrors = require('./src/middleware/handleErrors');
 
-const homeHandler = require('./handlers/homeHandler');
-const addToolHandler = require('./handlers/addToolHandler');
-const addToolPostHandler = require('./handlers/addToolPostHandler');
-const signupHandler = require('./handlers/signupPageHandler');
-const signupPostHandler = require('./handlers/signupPostHandler');
-const signinHandler = require('./handlers/signinPageHandler');
-const signinPostHandler = require('./handlers/signinPostHandler');
-const signoutHandler = require('./handlers/signoutHandler');
-const missingHandler = require('./handlers/missingHandler');
+const homeHandler = require('./src/handlers/homeHandler');
+const addToolHandler = require('./src/handlers/addToolHandler');
+const addToolPostHandler = require('./src/handlers/addToolPostHandler');
+const signupHandler = require('./src/handlers/signupPageHandler');
+const signupPostHandler = require('./src/handlers/signupPostHandler');
+const signinHandler = require('./src/handlers/signinPageHandler');
+const signinPostHandler = require('./src/handlers/signinPostHandler');
+const signoutHandler = require('./src/handlers/signoutHandler');
+const missingHandler = require('./src/handlers/missingHandler');
 
 const PORT = process.env.PORT || 3000;
 
